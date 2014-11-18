320eb1d676102cbfcb0006108e39e4504f6c74e5
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2014-11-18 08:55:42 -0200

    Correção da chamada do plugin.

--- www/WebPEncoderDecoder.js
+++ www/WebPEncoderDecoder.js
@@ -6,13 +6,13 @@ var argscheck = require('cordova/argscheck'),
 var WebPEncoderDecoder = function() {
 };
 
-WebPEncoderDecoder.prototype.encode = function(data, successCallback, errorCallback) {
+WebPEncoderDecoder.encode = function(data, successCallback, errorCallback) {
     return exec(successCallback, errorCallback, "WebPEncoderDecoder", "encode", [{
     	"data": data
     }]);
 };
 
-WebPEncoderDecoder.prototype.decode = function(data, successCallback, errorCallback) {
+WebPEncoderDecoder.decode = function(data, successCallback, errorCallback) {
     return exec(successCallback, errorCallback, "WebPEncoderDecoder", "decode", [{
     	"data": data
     }]);
